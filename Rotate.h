4a2ecd2bb2a6e4e1e7302198c8c95112a72b237a
Lee seung chan <dltmdcks702@naver.com>
2017-12-05 13:51:03 +0900

    add standard header file on Rotate.h

--- Rotate.h
+++ Rotate.h
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 char *TrivialPricySolution(char input[], int d);
-char *jugglng(char input[], int d);
+char *juggling(char input[], int d);
 char *bw(char input[], int d);
-char *reverse(char input[], int d);
+char *Reversal_Algorithm(char input[], int d);
